f6c112340f4d52b2bdb0d2f508684c884542720b
Luke Berry <contact@luke-berry.co.uk>
2018-07-03 00:13:39 +0100

    add basic game components
    - rotatable cannonpipe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import './index.css'
+import Game from './containers/Game'
+import reducer from './reducers'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+/* eslint-enable */
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: `aint nothin but a g thang`
+}
+
+function reducer (state = initialState) {
+  return state
+}
+
+export default reducer
